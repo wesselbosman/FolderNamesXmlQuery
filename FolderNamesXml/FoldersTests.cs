d0745a0f401f0927a0dc93244734eee2e3b830f9
Wessel Bosman <wessel.bosman@hotmail.com>
2018-02-24 16:46:07 +0000

    Initial Commit

---
+++ FolderNamesXml/FoldersTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace FolderNamesXml
+{
+	[TestFixture]
+	public class FoldersTests
+	{
+		[Test]
+		public void FolderNames_GivenXmlAndStartingLetter_ShouldReturnFoldersStartingWithStartingLetter()
+		{
+			// Arrange
+			string xml =
+				"<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+				"<folder name=\"c\">" +
+				"<folder name=\"program files\">" +
+				"<folder name=\"uninstall information\" />" +
+				"</folder>" +
+				"<folder name=\"users\" />" +
+				"</folder>";
+
+			// Act
+			var folderNames = Folders.FolderNames(xml, 'u');
+
+			// Assert
+			Assert.That(folderNames.Count(), Is.EqualTo(2));
+		}
+	}
+}
---
+++ FolderNamesXml/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+[Serializable, XmlRoot("folder")]
+public class Folder
+{
+	[XmlAttribute("name")]
+	public string Name { get; set; }
+
+	[XmlElement("folder")]
+	public List<Folder> SubFolders { get; set; }
+
+	public bool StartsWith(string startingString)
+	{
+		return Name.StartsWith(startingString);
+	}
+
+	public bool StartsWith(char startingChar)
+	{
+		return StartsWith(startingChar.ToString());
+	}
+}
+
+public class Folders
+{
+	public static IEnumerable<string> FolderNames(string xml, char startingLetter)
+	{
+		var xmlSerializer = new XmlSerializer(typeof(Folder));
+		var folders = (Folder)xmlSerializer.Deserialize(new StringReader(xml));
+
+		var foldersMatchingCharacter = new List<string>();
+		if (folders.StartsWith(startingLetter))
+		{
+			foldersMatchingCharacter.Add(folders.Name);
+		}
+		foldersMatchingCharacter.AddRange(folders.SubFolders.Where(x => x.StartsWith(startingLetter)).Select(x => x.Name));
+		foldersMatchingCharacter.AddRange(folders.SubFolders.SelectMany(x => x.SubFolders).Where(x => x.StartsWith(startingLetter)).Select(x => x.Name));
+
+		return foldersMatchingCharacter;
+	}
+}
+
